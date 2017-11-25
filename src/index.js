02675e340bfbf059133bc115b51f5900c145fd77
Pier-Luc Gendreau <pierluc@classcraft.com>
2017-11-25 17:25:38 -0500

    chore(deps): switch to better unicode module
    Closes #27

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function symbols (code) {
     return _symbols[code]
   }
 
-  _symbols = require('unicode-json/category/So')
+  _symbols = require('unicode/category/So')
   removelist = ['sign', 'cross', 'of', 'symbol', 'staff', 'hand', 'black', 'white'].map(function (word) {
     return new RegExp(word, 'gi')
   })
